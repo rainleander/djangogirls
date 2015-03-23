111d7fc961e0d26ca47c709264631e8b32f82400
Rain <rainsdance@gmail.com>
2015-03-23 12:11:58 +0100

    Add DELETE function

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns('',
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
     url(r'^post/(?P<pk>[0-9]+)/publish/$', views.post_publish, name='post_publish'),
+    url(r'^post/(?P<pk>[0-9]+)/remove/$', views.post_remove, name='post_remove'),
 )
--- blog/views.py
+++ blog/views.py
@@ -46,3 +46,8 @@ def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('blog.views.post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
