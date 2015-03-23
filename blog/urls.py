9bce497c6e445f56723e9e82836610802c63924a
Rain <rainsdance@gmail.com>
2015-03-23 12:04:00 +0100

    Add PUBLISH function

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = patterns('',
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+    url(r'^post/(?P<pk>[0-9]+)/publish/$', views.post_publish, name='post_publish'),
 )
